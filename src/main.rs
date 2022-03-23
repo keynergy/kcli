b7dabb619d90c081d52c63ee8d4b2cbf74004add
semilin <semilinares@pm.me>
2022-03-22 21:49:36 -0700

    add corpus

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 mod config;
 mod keyboards;
-mod load_metrics;
+//mod load_metrics;
 mod setup;
 use clap::{Parser, Subcommand};
 pub use config::Config;
 pub use keyboards::{ansi, matrix};
 pub use setup::setup;
+use std::path::PathBuf;
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -19,8 +20,17 @@ struct Cli {
 enum Commands {
     /// Set up data directory
     Setup { dir: Option<String> },
-    /// Pre-analyses keyboard metric data
-    LoadMetrics,
+    Corpus {
+        #[clap(subcommand)]
+        command: CorpusCommand,
+    },
+}
+
+#[derive(Debug, Subcommand)]
+enum CorpusCommand {
+    List,
+    Use { corpus: String },
+    Load { file: PathBuf },
 }
 
 fn do_setup() -> Config {
@@ -39,6 +49,8 @@ fn main() {
         Commands::Setup { dir } => {
             setup(dir);
         }
-        Commands::LoadMetrics => {}
+        Commands::Corpus { command } => {
+            println!("{:?}", command);
+        }
     }
 }
