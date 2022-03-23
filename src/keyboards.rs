e2950490f38d73b8f043f921cfb6bb96712cfb85
semilin <semilinares@pm.me>
2022-03-22 21:40:38 -0700

    fix

--- src/keyboards.rs
+++ src/keyboards.rs
@@ -1,11 +1,11 @@
 use keynergy::fingers::*;
-use keynergy::{Fingermap, Keyboard};
+use keynergy::Keyboard;
 
 pub fn ansi() -> Keyboard {
     Keyboard {
-        name: "ANSI",
-        row_stagger: vec![0.0, 0.25, 0.75],
-        col_stagger: vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        name: "ANSI".to_string(),
+        rowstagger: vec![0.0, 0.25, 0.75],
+        colstagger: vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
         dimensions: [11, 3],
         keyheight: 1.0,
         fingers: vec![
@@ -18,9 +18,9 @@ pub fn ansi() -> Keyboard {
 
 pub fn matrix() -> Keyboard {
     Keyboard {
-        name: "Matrix",
-        row_stagger: vec![0.0, 0.0, 0.0],
-        col_stagger: vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        name: "Matrix".to_string(),
+        rowstagger: vec![0.0, 0.0, 0.0],
+        colstagger: vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
         dimensions: [11, 3],
         keyheight: 1.0,
         fingers: vec![
